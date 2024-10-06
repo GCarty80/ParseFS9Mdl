5b36dd4821f4c97dc2971c279b39743ffce5a625
George Carty <georgecarty43@gmail.com>
2024-10-06 19:58:58 +0100

    starting to decode the DICT section

--- ParseFS9Mdl.py
+++ ParseFS9Mdl.py
@@ -3,6 +3,8 @@ import json
 
 mdl_dir_to_parse = "D:\\Flight\\FSX\\SimObjects\\Airplanes\\HJG Boeing KC-135A Early v6.1 BETA\\model.KC-135A_Early"
 
+var_type_from_number = {0:"CUSTOM", 1:"FLOAT32", 2:"UINT32", 4:"UINT16"}
+
 class BadBlockException(Exception):
     pass
 
@@ -11,6 +13,9 @@ def hexDump(bytes_to_dump):
     for byte in bytes_to_dump:
         output = "%s%02x"%(output, byte)
     return output
+    
+def makeGuid(input_bytes):
+    return "%08X-%04X-%04X-%04X-%012X"%(int.from_bytes(input_bytes[:4], "little"), int.from_bytes(input_bytes[4:6], "little"), int.from_bytes(input_bytes[6:8], "little"), int.from_bytes(input_bytes[8:10], "big"), int.from_bytes(input_bytes[10:], "big"))
 
 def parseBytes(input_bytes):
     output = []
@@ -30,10 +35,23 @@ def parseBytes(input_bytes):
             continue
         elif block_id == "DICT":
             records = []
+            custom_record_string = None
             for record in range(block_size//28):
                 record_bytes = input_bytes[8+record*28:36+record*28]
-                records.append({"type":int.from_bytes(record_bytes[:4], "little"), "offset":int.from_bytes(record_bytes[4:8], "little"),"spacing":int.from_bytes(record_bytes[8:12], "little"), "id":hexDump(record_bytes[12:])})
-            output.append({"block":"DICT", "records":records})
+                type_no = var_type_from_number[int.from_bytes(record_bytes[:4], "little")]
+                if type_no == "CUSTOM":
+                    if custom_record_string is None:
+                        custom_record_string = ""
+                    custom_record_string += record_bytes[4:].decode("latin1")
+                else:
+                    if custom_record_string is not None:
+                        records.append({"type":"CUSTOM", "string":custom_record_string.rstrip("\00")})
+                        custom_record_string = None
+                    records.append({"type":type_no, "offset":int.from_bytes(record_bytes[4:8], "little"),"spacing":int.from_bytes(record_bytes[8:12], "little"), "id":makeGuid(record_bytes[12:])})
+            if custom_record_string is not None:
+                records.append({"type":"CUSTOM", "string":custom_record_string.rstrip("\00")})
+                custom_record_string = None
+            output.append({"block":"DICT", "num_entries":len(records), "records":records})
             del input_bytes[:8+block_size]
             continue
         elif block_id == "BBOX":
